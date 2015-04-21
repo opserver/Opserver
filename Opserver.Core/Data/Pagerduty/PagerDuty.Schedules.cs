69ca798c2f86bb04b2582876b2c16f0d9f001248
george <george@stackoverflow.com>
2015-04-21 19:14:27 -0400

    Add initial code for schedule manipulation to Core

---
+++ Opserver.Core/Data/Pagerduty/PagerDuty.Schedules.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Jil;
+
+namespace StackExchange.Opserver.Data.PagerDuty
+{
+    public partial class PagerDutyApi
+    {
+
+        public string PrimaryScheduleId
+        {
+            get { return AllSchedules.Data.Find(s => s.Name == "Primary On Call").Id; }
+        }
+        private Cache<List<PagerDutySchedule>> _schedules;
+
+        public Cache<List<PagerDutySchedule>> AllSchedules
+        {
+            get
+            {
+                return _schedules ?? (_schedules = new Cache<List<PagerDutySchedule>>()
+                {
+                    CacheForSeconds = 10 * 60,
+                    UpdateCache = UpdateCacheItem(
+                        description: "Pager Duty Incidents",
+                        getData: GetAllSchedules,
+                        logExceptions: true
+                        )
+                });
+            }
+        }
+
+        private List<PagerDutySchedule> GetAllSchedules()
+        {
+            return GetFromPagerDuty("schedules", getFromJson:
+                response => JSON.Deserialize<PagerDutyScheduleResponse>(response.ToString(), Options.ISO8601).Schedules);
+        }
+
+
+    }
+
+    class PagerDutyScheduleResponse
+    {
+        [DataMember(Name="schedules")]
+        public List<PagerDutySchedule> Schedules { get; set; }
+    }
+
+    public class PagerDutySchedule
+    {
+        [DataMember(Name="id")]
+        public string Id { get; set; }
+        [DataMember(Name="name")]
+        public string Name { get; set; }
+        [DataMember(Name="time_zone")]
+        public string TimeZone { get; set; }
+    }
+}
