f41e9bed763f7f70147bbb3f9beafab06896b5d9
Nick Craver <craver@stackoverflow.com>
2016-01-04 14:47:05 -0500

    Remove System.Web.UI.DataVisualization.Charting library - redoing HAProxy.Traffic anyway.

--- Opserver/Controllers/GraphController.HAProxy.cs
+++ Opserver/Controllers/GraphController.HAProxy.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Drawing;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
-using System.Web.UI.DataVisualization.Charting;
 using StackExchange.Opserver.Data.HAProxy;
 
 namespace StackExchange.Opserver.Controllers
 {
     public partial class GraphController
     { 
-        private static readonly Color AltRouteBackground = ColorTranslator.FromHtml("#fafafa");
-        
         [OutputCache(Duration = 60 * 60, VaryByParam = "host;start;end;summary", VaryByContentEncoding = "gzip;deflate")]
         [Route("graph/haproxy/traffic/json")]
         public async Task<ActionResult> HAProxyTrafficJson(string host, long start, long end, bool? summary = false)
@@ -50,30 +45,7 @@ namespace StackExchange.Opserver.Controllers
         {
             var dataPoints = await HAProxyTraffic.GetRouteDataAsync(route, days, host: host);
 
-            var chart = GetChart(height, width);
-            chart.BackColor = alt ? AltRouteBackground : System.Drawing.Color.White;
-            var area = GetRouteChartArea(alt);
-            if (dataPoints.Count >= 2)
-            {
-                area.AxisX.Minimum= dataPoints.First().CreationDate.ToOADate();
-                area.AxisX.Maximum = dataPoints.Last().CreationDate.ToOADate();
-            }
-
-            var hits = new Series("Total Hits")
-            {
-                ChartType = SeriesChartType.StackedArea,
-                XValueType = ChartValueType.DateTime,
-                Color = ColorTranslator.FromHtml("#c6d5e2")
-            };
-            chart.Series.Add(hits);
-
-            foreach (var rt in dataPoints)
-            {
-                hits.Points.Add(new DataPoint(rt.CreationDate.ToOADate(), rt.Hits.GetValueOrDefault(0)));
-            }
-
-            chart.ChartAreas.Add(area);
-            return chart.ToResult();
+            return Json(dataPoints);
         }
 
         //[OutputCache(Duration = 20 * 60, VaryByParam = "route;days;host;height;width;alt", VaryByContentEncoding = "gzip;deflate")]
@@ -82,58 +54,7 @@ namespace StackExchange.Opserver.Controllers
         {
             var dataPoints = await HAProxyTraffic.GetRouteDataAsync(route, days, host: host);
 
-            var chart = GetChart(height, width);
-            chart.BackColor = alt ? AltRouteBackground : System.Drawing.Color.White;
-            var area = GetRouteChartArea(alt);
-            if (dataPoints.Count >= 2)
-            {
-                area.AxisX.Minimum = dataPoints.First().CreationDate.ToOADate();
-                area.AxisX.Maximum = dataPoints.Last().CreationDate.ToOADate();
-            }
-
-            area.AxisY.StripLines.Add(new StripLine
-            {
-                BackColor = ColorTranslator.FromHtml("#22FFFFFF"),
-                StripWidth = 25,
-                Interval = 2 * 25,
-                IntervalOffset = 0
-            });
-
-            Func<string, string, Series> getSeries = (name, color) =>
-                {
-                    var tColor = ColorTranslator.FromHtml(color);
-                    //var bColor = Color.FromArgb(200, tColor);
-                    var result = new Series(name)
-                        {
-                            ChartType = SeriesChartType.StackedArea,
-                            XValueType = ChartValueType.DateTime,
-                            //BackGradientStyle = GradientStyle.TopBottom,
-                            Color = tColor,
-                            //BackSecondaryColor = bColor
-                        };
-                    chart.Series.Add(result);
-                    return result;
-                };
-
-            var tAsp = getSeries("ASP.Net", "#0E2A4C");
-            var tSql = getSeries("SQL", "#143D65");
-            var tRedis = getSeries("Redis", "#194D79");
-            var tHTTP = getSeries("HTTP", "#1D5989");
-            var tTagEngine = getSeries("Tag Engine", "#206396");
-            var tOther = getSeries("Other", "#64B6D0");
-            
-            foreach (var rt in dataPoints)
-            {
-                tAsp.Points.Add(new DataPoint(rt.CreationDate.ToOADate(), (double)rt.AvgCalculatedAspNetDurationMs.GetValueOrDefault(0)));
-                tSql.Points.Add(new DataPoint(rt.CreationDate.ToOADate(), (double)rt.AvgSqlDurationMs.GetValueOrDefault(0)));
-                tRedis.Points.Add(new DataPoint(rt.CreationDate.ToOADate(), (double)rt.AvgRedisDurationMs.GetValueOrDefault(0)));
-                tHTTP.Points.Add(new DataPoint(rt.CreationDate.ToOADate(), (double)rt.AvgHttpDurationMs.GetValueOrDefault(0)));
-                tTagEngine.Points.Add(new DataPoint(rt.CreationDate.ToOADate(), (double)rt.AvgTagEngineDurationMs.GetValueOrDefault(0)));
-                tOther.Points.Add(new DataPoint(rt.CreationDate.ToOADate(), (double)(rt.AvgCalculatedOtherDurationMs.GetValueOrDefault(0))));
-            }
-
-            chart.ChartAreas.Add(area);
-            return chart.ToResult();
+            return Json(dataPoints);
         }
 
         [Route("graph/haproxy/route-performance/json")]
@@ -166,38 +87,6 @@ namespace StackExchange.Opserver.Controllers
                             hits = dp.Hits
                         })
                 });
-        }
-
-        private static ChartArea GetRouteChartArea(bool alt)
-        {
-            var area = new ChartArea("area")
-            {
-                BackColor = alt ? AltRouteBackground : System.Drawing.Color.White,
-                Position = new ElementPosition(0, 0, 100, 100),
-                InnerPlotPosition = new ElementPosition(0, 0, 100, 100),
-                AxisY =
-                {
-                    Minimum = 0,
-                    MaximumAutoSize = 100,
-                    LabelStyle = { Enabled = true },
-                    Interval = 10,
-                    IntervalAutoMode = IntervalAutoMode.VariableCount,
-                    MajorGrid = { Enabled = false },
-                    MajorTickMark = { Enabled = false },
-                    LineWidth = 0,
-                    LineDashStyle = ChartDashStyle.Dot,
-                },
-                AxisX =
-                {
-                    MaximumAutoSize = 100,
-                    LabelStyle = { Enabled = false },
-                    LineWidth = 0,
-                    MajorTickMark = { Enabled = false },
-                    MajorGrid = { Enabled = false }
-                }
-            };
-            
-            return area;
         }
     }
 }
--- Opserver/Controllers/GraphController.cs
+++ Opserver/Controllers/GraphController.cs
@@ -1,49 +1,6 @@
-﻿using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Web.Mvc;
-using System.Web.UI.DataVisualization.Charting;
-using System.Web.UI.WebControls;
-
-namespace StackExchange.Opserver.Controllers
+﻿namespace StackExchange.Opserver.Controllers
 {
     public partial class GraphController : StatusController
-    {   
-        private static Chart GetChart(int height, int width)
-        {
-            return new Chart
-                       {
-                           BackColor = System.Drawing.Color.Transparent,
-                           Width = Unit.Pixel(width),
-                           Height = Unit.Pixel(height),
-                           AntiAliasing = AntiAliasingStyles.All,
-                           TextAntiAliasingQuality = TextAntiAliasingQuality.High,
-                           Palette = ChartColorPalette.None
-                       };
-        }
-    }
-
-    public static class ChartExtentions
     {
-        public static ActionResult ToResult(this Chart chart)
-        {
-            var width = (int)chart.Width.Value;
-            var height = (int)chart.Height.Value;
-
-            using (var bmp = new Bitmap(width, height))
-            {
-                bmp.SetResolution(326, 326); // retina max
-                using (var g = Graphics.FromImage(bmp))
-                {
-                    chart.Paint(g, new Rectangle(new Point(0, 0), new Size(width, height)));
-                }
-                using (var ms = new MemoryStream())
-                {
-                    bmp.Save(ms, ImageFormat.Png);
-                    ms.Seek(0, SeekOrigin.Begin);
-                    return new FileContentResult(ms.ToArray(), "image/png");
-                }
-            }
-        }
     }
 }
