8eb2d21ac5653b3c4d22926f253cfe8a94c06a96
Frits Duus Jørgensen <fdj@proshop.eu>
2016-02-19 22:22:25 +0100

    Removed unused using statement

--- Opserver.Core/Data/Dashboard/Providers/WmiDataProvider.Polling.cs
+++ Opserver.Core/Data/Dashboard/Providers/WmiDataProvider.Polling.cs
@@ -3,7 +3,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Management;
-using System.Net;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
