0abe8c9566952f4c5be17868b617b560d1af0ebd
Nick Craver <nrcraver@gmail.com>
2016-04-03 09:19:22 -0400

    WMI: settings cleanup

--- Opserver.Core/Settings/DashboardSettings.WMI.cs
+++ Opserver.Core/Settings/DashboardSettings.WMI.cs
@@ -11,22 +11,22 @@ namespace StackExchange.Opserver
         /// <summary>
         /// List of hostnames to poll via WMI
         /// </summary>
-        public List<string> Nodes { get; set; }
+        public List<string> Nodes { get; set; } = new List<string>();
 
         /// <summary>
         /// Timeout in seconds for cache with more or less static data, like node name or volume size.
         /// </summary>
-        public int StaticDataTimeoutSeconds { get; set; }
+        public int StaticDataTimeoutSeconds { get; set; } = 5 * 60;
 
         /// <summary>
         /// Timeout in seconds for dynamic data like CPU load.
         /// </summary>
-        public int DynamicDataTimeoutSeconds { get; set; }
+        public int DynamicDataTimeoutSeconds { get; set; } = 30;
 
         /// <summary>
         /// Amount of history to keep
         /// </summary>
-        public int HistoryHours { get; set; }
+        public int HistoryHours { get; set; } = 24;
 
         /// <summary>
         /// Username to use when polling (for non-domain testing)
@@ -37,13 +37,7 @@ namespace StackExchange.Opserver
         /// </summary>
         public string Password { get; set; }
 
-        public WMISettings()
-        {
-            Nodes = new List<string>();
-            StaticDataTimeoutSeconds = 60 * 5;
-            DynamicDataTimeoutSeconds = 5;
-            HistoryHours = 2;
-        }
+        public WMISettings() {}
 
         public void Normalize() {}
     }
