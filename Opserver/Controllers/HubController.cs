b21e12281f5dc31fcc5212d032ab3d028cbf327f
Nick Craver <nrcraver@gmail.com>
2016-04-07 08:31:46 -0400

    Start playing with /hub layout ideas

--- Opserver/Controllers/HubController.cs
+++ Opserver/Controllers/HubController.cs
@@ -1,11 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
-using StackExchange.Opserver.Data;
-using StackExchange.Opserver.Data.Dashboard;
-using StackExchange.Opserver.Data.Elastic;
-using StackExchange.Opserver.Data.HAProxy;
-using StackExchange.Opserver.Data.SQL;
+﻿using System.Web.Mvc;
 using StackExchange.Opserver.Helpers;
 using StackExchange.Opserver.Models;
 using StackExchange.Opserver.Views.Hub;
@@ -19,27 +12,7 @@ namespace StackExchange.Opserver.Controllers
         [Route("hub"), Route("headsup"), AlsoAllow(Roles.InternalRequest)]
         public ActionResult Index()
         {
-            var items = new List<IMonitorStatus>();
-
-            var nodes = DashboardData.AllNodes
-                                        .Where(s => !s.IsUnwatched)
-                                        .Where(s => s.CPULoad >= 0 || s.MemoryUsed >= 0)
-                                        .OrderByWorst(s => s.CPUMonitorStatus())
-                                        .ThenByWorst(s => s.MemoryMonitorStatus())
-                                        .ThenBy(s => s.Category.Index)
-                                        .ThenBy(s => s.PrettyName);
-            // Dashboard
-            items.AddRange(nodes);
-            items.AddRange(SQLCluster.AllClusters.SelectMany(c => c.AvailabilityGroups));
-            // TODO: Redis
-            items.AddRange(ElasticCluster.AllClusters.OrderByWorst().ThenBy(c => c.Name)); 
-            // TODO: Exceptions
-            items.AddRange(HAProxyGroup.AllGroups.OrderByWorst().ThenBy(g => g.Name));
-
-            var vd = new HubModel
-                {
-                    Items = items
-                };
+            var vd = new HubModel();
             return View(vd);
         }
     }
