8926859ddffc147f36cbe29911fc3d1dfcc122f5
Nick Craver <nrcraver@gmail.com>
2017-01-08 15:14:02 -0500

    Icons: for redis and centralize common definitions.
    TODO: Figure out an elegant way to do color classes. This is an
    excellent ASP.NET MVC 6 tag helper use case.

---
+++ Opserver/Helpers/Icon.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+
+namespace StackExchange.Opserver.Helpers
+{
+    public class Icon
+    {
+        public static IHtmlString Cache = GetIcon("usd");
+        public static IHtmlString Cluster = GetIcon("share-alt");
+        public static IHtmlString Cog = GetIcon("cog");
+        public static IHtmlString Database = GetIcon("database");
+        public static IHtmlString Disk = GetIcon("hdd-o");
+        public static IHtmlString Download = GetIcon("arrow-circle-o-down");
+        public static IHtmlString Memory = GetIcon("microchip");
+        public static IHtmlString Network = GetIcon("exchange");
+        public static IHtmlString Performance = GetIcon("bar-chart");
+        public static IHtmlString Refresh = GetIcon("refresh");
+        public static IHtmlString Server = GetIcon("server");
+        public static IHtmlString StackOverflow = GetIcon("stack-overflow");
+        public static IHtmlString Stats = GetIcon("area-chart");
+        public static IHtmlString Time = GetIcon("clock-o");
+        public static IHtmlString Upload = GetIcon("arrow-circle-o-up");
+        public static IHtmlString Users = GetIcon("users");
+        public static IHtmlString VM = GetIcon("cloud");
+        public static IHtmlString X = GetIcon("times");
+
+
+        private static IHtmlString GetIcon(string className) => 
+            $@"<i class=""fa fa-{className}"" aria-hidden=""true""></i>".AsHtml();
+    }
+}
