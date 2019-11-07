453ee52b6037329a529582a03b36fd526c3bf7df
Nick Craver <craver@stackoverflow.com>
2019-11-06 22:32:03 -0500

    Spin up test project

---
+++ tests/Opserver.Tests/SecurityProviderTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Opserver.Security;
+using Xunit;
+
+namespace Opserver.Tests
+{
+    public class SecurityProviderTests
+    {
+        [Fact]
+        public void ActiveDirectoryProvider()
+        {
+            var memCache = new MemoryCache(new MemoryCacheOptions());
+            var options = Options.Create(new SecuritySettings
+            {
+                Provider = "ActiveDirectory"
+            });
+            var sm = new SecurityManager(options, memCache);
+            Assert.IsType<ActiveDirectoryProvider>(sm.CurrentProvider);
+        }
+
+        [Fact]
+        public void EveryonesAnAdminProvider()
+        {
+            var memCache = new MemoryCache(new MemoryCacheOptions());
+            var options = Options.Create(new SecuritySettings
+            {
+                Provider = "EveryonesAnAdmin"
+            });
+            var sm = new SecurityManager(options, memCache);
+            Assert.IsType<EveryonesAnAdminProvider>(sm.CurrentProvider);
+        }
+
+        [Fact]
+        public void DefaultProvider()
+        {
+            var memCache = new MemoryCache(new MemoryCacheOptions());
+            var options = Options.Create(new SecuritySettings());
+            var sm = new SecurityManager(options, memCache);
+            Assert.IsType<EveryonesReadOnlyProvider>(sm.CurrentProvider);
+
+            var bsOptions = Options.Create(new SecuritySettings()
+            {
+                Provider = "admjnfajkfnajsklfnasfd"
+            });
+            var bsSm = new SecurityManager(bsOptions, memCache);
+            Assert.IsType<EveryonesReadOnlyProvider>(bsSm.CurrentProvider);
+        }
+    }
+}
