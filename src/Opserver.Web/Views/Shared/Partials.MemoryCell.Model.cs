9590494d4f4da09d4b3b162f6b8da1dee1778aa7
Nick Craver <craver@stackoverflow.com>
2020-02-07 22:52:05 -0500

    Fix models on SQL memory guages

---
+++ src/Opserver.Web/Views/Shared/Partials.MemoryCell.Model.cs
@@ -0,0 +1,13 @@
+﻿using Opserver.Data.SQL;
+
+namespace Opserver.Views.Shared
+{
+    public class PartialsMemoryCellModel
+    {
+        public SQLInstance Instance { get; }
+        public int DecimalPlaces { get; }
+
+        public PartialsMemoryCellModel(SQLInstance instance, int decimalPlaces = 0) =>
+            (Instance, DecimalPlaces) = (instance, decimalPlaces);
+    }
+}
