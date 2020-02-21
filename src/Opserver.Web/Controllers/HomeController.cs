3afd6ef2530f5f6f74b42ce3354c71054901e255
Nick Craver <craver@stackoverflow.com>
2020-02-21 12:39:48 -0500

    Force auth to all controllers

--- src/Opserver.Web/Controllers/HomeController.cs
+++ src/Opserver.Web/Controllers/HomeController.cs
@@ -17,6 +17,7 @@ using System.Collections.Generic;
 
 namespace Opserver.Controllers
 {
+    [OnlyAllow(Roles.Authenticated)]
     public class HomeController : StatusController
     {
         private PollingService Poller { get; }
