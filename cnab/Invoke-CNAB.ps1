64fc111ebd5f43bfbfd0d9ed95e69bb3a7739e14
Stephen Vakil <svakil@stackoverflow.com>
2024-06-28 13:45:44 -0500

    CNAB (only works locally)

---
+++ cnab/Invoke-CNAB.ps1
@@ -0,0 +1,138 @@
+[CmdletBinding()]
+param (
+    [ValidateSet("pre-install", "install")]
+    [string]
+    $Action = "install",
+    [bool]
+    $RunAsContainer = $false,
+    [ValidateSet("GCP", "DockerDesktop")]
+    [string]
+    $target = "GCP",
+    [string]
+    $CNABImage = "stackmail-cnab:local"
+)
+
+# Function to check if a command exists
+function Check-CommandExists {
+    param (
+        [string]$Command
+    )
+
+    if ($null -eq (Get-Command "$Command.exe" -ErrorAction SilentlyContinue)) { 
+        Write-Host "Unable to find $Command. Please install $Command before continuing"
+        exit 1
+    }
+}
+
+function Setup-GCP {
+    Check-CommandExists -Command "gcloud-crc32c"
+    
+    # Check if user is signed in to gcloud
+    $gcloudAuthStatus = gcloud auth list --format="value(account)"
+    if (-not $gcloudAuthStatus) {
+        & gcloud auth login
+    }
+}
+
+function Setup-DockerDesktop {
+    Check-CommandExists -Command "kubectl"
+    Check-CommandExists -Command "helm"
+
+    & kubectl config use-context "docker-desktop"
+
+    Write-Host "Check if Kubernetes is enabled in Docker Desktop"
+    $output = & kubectl get node docker-desktop  2>&1 | Out-String
+    
+    if ( $output.Trim() -eq "Unable to connect to the server: EOF") {
+        Write-Error "Kubernetes is not enabled in Docker Desktop. Please enable Kubernetes before continuing or change the target."
+        exit 1
+    }
+
+    Write-Host "Kubernetes is enabled in Docker Desktop"
+    Write-Host "Making sure prerequisites are installed"
+
+    $repoExists = (helm repo list | Select-String -Pattern "external-secrets")
+    $chartExists = (helm list -n external-secrets | Select-String -Pattern "external-secrets")
+
+    if (-not $repoExists) {
+        Write-Host "Adding external-secrets repo to Kubernetes"
+        & helm repo add external-secrets https://charts.external-secrets.io
+    }
+
+    if (-not $chartExists) {
+        Write-Host "Adding external-secrets chart to Kubernetes"
+        & helm install external-secrets external-secrets/external-secrets -n external-secrets --create-namespace --set installCRDs=true
+    }
+}
+
+$MetaJsonPath = "$PSScriptRoot/app/variables.$target.json"
+
+if (-not (Test-Path $MetaJsonPath)) {
+    Write-Error "File not found: $MetaJsonPath"
+    exit 1
+}
+
+if ($target -eq "DockerDesktop") {
+    Setup-DockerDesktop
+}
+elseif ($target -eq "GCP") {
+    Setup-GCP
+}
+
+if ($RunAsContainer) {
+    # Build a local copy of CNAB image
+    docker build -t $CNABImage -f $PSScriptRoot/build/Dockerfile .
+
+    $dockerRunArgs = @()
+
+    if ($target -eq "GCP") {
+        # Get current config path
+        $gcloudConfigDir = gcloud info --format='value(config.paths.global_config_dir)'
+        $dockerRunArgs += @(
+            "-v", "$($gcloudConfigDir):/root/.config/gcloud",
+            "--env", "GOOGLE_APPLICATION_CREDENTIALS=/gcp/creds.json"
+        )
+    }
+    elseif ($target -eq "DockerDesktop") {
+
+        if ($IsWindows) {
+            $kubeConfigPath = "$env:USERPROFILE\.kube\config"
+        } else {
+            $kubeConfigPath = "~/.kube/config"
+        }
+
+        $dockerRunArgs += @(
+            "-v", "$($kubeConfigPath):/.kube/config:ro",
+            "--env", "KUBECONFIG=/.kube/config"
+        )
+    }
+    
+    $dockerRunArgs += @(
+        "-v", "$($MetaJsonPath):/variables.json",
+        "--env", "CNAB_ACTION=$Action",
+        "--env", "INSTALLATION_METADATA=/variables.json",
+        "--rm", "$CNABImage", "/cnab/app/run.ps1"
+    )    
+
+    docker run $dockerRunArgs
+}
+else {
+    Check-CommandExists -Command "kubectl"
+
+    $env:CNAB_ACTION = "install"
+    $env:INSTALLATION_METADATA = $MetaJsonPath
+    
+    if ($target -eq "GCP") {
+        $vars = (Get-Content $env:INSTALLATION_METADATA | ConvertFrom-Json)
+        $tenantData = $vars.tenant_metadata
+        $contextName = "gke_" + $tenantData.project + "_" + $tenantData.region + "_" + $tenantData.gke_cluster_name
+
+        & kubectl config use-context $contextName
+    }
+    elseif ($target -eq "DockerDesktop") {
+        & kubectl config use-context "docker-desktop"
+    }   
+
+    & ".\cnab\app\run.ps1" -runAsContainer $false
+}
+
---
+++ cnab/app/run.ps1
@@ -0,0 +1,234 @@
+#!/usr/bin/env pwsh
+# Strict Mode
+param(
+  [bool]$runAsContainer = $true
+)
+
+#Requires -Version 7.4
+Set-StrictMode -Version 3.0
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
+
+# Trap to ensure errors get shown as errors in Octopus Deploy, otherwise they'd go to Verbose because the
+# 00-cnab-base.sh script sets '##octopus[stderr-progress]' to avoid all stderr showing up as errors.
+# We'll assume we're running under Octopus Deploy if the error happens before we've determined $IsOctopusDeploy.
+trap {
+  if (!(Test-Path Variable:\IsOctopusDeploy) -or $IsOctopusDeploy) {
+    Write-Host '##octopus[stderr-default]'
+  }
+  break
+}
+
+Write-Output "$($PSStyle.Bold)Installing opserver...$($PSStyle.BoldOff)"
+
+$action = $env:CNAB_ACTION
+
+if (-not $env:INSTALLATION_METADATA) {
+  throw "INSTALLATION_METADATA is not set"
+}
+
+Write-Verbose "INSTALLATION_METADATA is set to '$env:INSTALLATION_METADATA'"
+$vars = (Get-Content $env:INSTALLATION_METADATA | ConvertFrom-Json) 
+. $PSScriptRoot/utils.ps1
+
+if ($runAsContainer) {
+  Write-Output "Running as container"
+}
+else {
+  Write-Output "Running locally"
+}
+
+$tenant = $vars.pipeline.tenant
+$environment = $vars.pipeline.environment
+$project = $vars.pipeline.project
+
+$releaseTag = $vars.pipeline.releaseTag
+
+# PR container images are located in `cr-dev` in CloudSmith. As opposed to `cr` which we use for release builds.
+$isPr = $releaseTag -match '^pr-[0-9]+$'
+if ($isPr) {
+  $containerRegistryUrl = 'crdev.stackoverflow.software'
+  $pullSecretName = 'cloudsmith-cr-dev'
+  $forceUpgrade = @('--force') # This'll force pods to be recreated with freshly-pulled images
+}
+else {
+  $containerRegistryUrl = 'cr.stackoverflow.software'
+  $pullSecretName = 'cloudsmith-cr-prod'
+  $forceUpgrade = @()
+}
+
+Write-Output "Container registry: $containerRegistryUrl"
+Write-Output "Pull secret name: $pullSecretName"
+
+function Invoke-WithEcho([string]$cmd) {
+  Write-Output "$($PSStyle.Dim)> $cmd $args$($PSStyle.Reset)"
+  & $cmd @args
+}
+
+Write-Output "Tool versions:"
+Invoke-WithEcho gcloud version 
+Invoke-WithEcho kubectl version --client=true
+Invoke-WithEcho helm version --short
+Invoke-WithEcho helm plugin list
+Invoke-WithEcho helm diff version
+
+# If the release tag starts with a commit hash plus dash "-", strip the extra characters. This lets us easily test Octopus pr's
+$releaseTag = $releaseTag -replace '([a-z0-9]{40})-.*', '$1'
+
+Write-MajorStep "Running $action for Tenant: $tenant - Environment: $environment - Project: $project in cloud: $($vars.runtime.name)"
+
+if ($vars.runtime.name -eq "GCP" -and $runAsContainer) {
+  
+  Write-MajorStep "Setting GCP cluster credentials"
+
+  # Default cluster cred args
+  $clusterCredArgs = @("container clusters get-credentials",
+    $vars.tenant_metadata.gke_cluster_name,
+    "--region", $vars.tenant_metadata.region,
+    "--project", $vars.tenant_metadata.project)
+  
+  # Get cluster credentials
+  Start-Process gcloud -ArgumentList $clusterCredArgs -NoNewWindow -Wait
+}
+
+switch ($action) {
+  "pre-install" { Write-Output "Pre-install action" }
+  "install" {
+    Write-MajorStep "Install action"
+
+    $app = 'opserver'
+    
+    $values = @{
+      tier                    = $environment
+      replicaCount            = $vars.vars.replicaCount
+      aspnetcoreEnvironment   = $vars.vars.aspnetcoreEnvironment
+      exceptionalDbName       = $vars.vars.exceptionalDbName;
+      product                 = "pubplat"
+
+      images                  = @{
+        containerRegistry = "$containerRegistryUrl"
+        opserver         = @{
+          tag = $releaseTag
+        }
+      }
+
+      requests                = @{
+        cpu    = $vars.vars.requestsCPU
+        memory = $vars.vars.requestsMemory
+      }
+  
+      limits                  = @{
+        memory = $vars.vars.limitsMemory
+      }
+  
+      podDisruptionBudget     = @{
+        minAvailable = $vars.vars.podDisruptionBudgetMinAvailable
+      }
+
+      exceptional             = @{
+        store = @{
+          type = $vars.vars.exceptionalStoreType
+        }
+      }
+
+      datadog                 = @{
+        agentHost = $vars.vars.datadogAgentHost
+        agentPort = $vars.vars.datadogAgentPort
+      }
+      
+      kestrel                 = @{
+        endPoints = @{
+          http = @{
+            url           = "http://0.0.0.0:8080/"
+            containerPort = "8080"
+          }
+        }
+      }
+
+      secretStore             = @{
+        fake = $vars.runtime.local
+      }
+
+      image                   = @{
+        pullSecretName = $pullSecretName
+      }
+
+      ingress                 = @{
+        className  = "nginx-external"
+        certIssuer = "letsencrypt-dns-prod"
+        host       = $vars.vars.opserverSettings.hostUrl
+        enabled    = $vars.vars.includeIngress
+        secretName = "opserver-tls"
+      }
+
+      sqlExternalSecret       = @{
+        name            = "opserver-sqldb-external-secret"
+        refreshInterval = "5m"
+        storeRefName    = $vars.vars.secretStore
+        targetName      = "sql-secret"
+        remoteRefs      = @{
+          exceptionalServerName = "ExceptionsSqlServerName"
+          exceptionalUsername   = "db-opserver-User"
+          exceptionalPassword   = "db-opserver-Password"
+        }
+      }
+
+      opserverSettings       = $vars.vars.opserverSettings
+
+      adminRolebindingGroupId = $vars.vars.adminRolebindingGroupId
+    }
+    
+    # Helm expects a YAML file but YAML is also a superset of JSON, so we can use ConvertTo-Json here
+    $valuesFileContent = $values | ConvertTo-Json -Depth 100
+    Write-Output "Populated Helm values:"
+    Write-Output $valuesFileContent
+
+    $tmpDir = [System.IO.Directory]::CreateTempSubdirectory($app + '-')
+    $valuesFilePath = (Join-Path $tmpDir.FullName 'populated-values.yml')
+    
+    $valuesFileContent > $valuesFilePath
+
+    $folder = Get-ChildItem -Path $env:PWD -Filter "charts" -Directory -Recurse | Select-Object -First 1
+    
+    if ($folder) {
+      $folder = $folder.FullName
+    }
+    else {
+      throw "No 'charts' folder found in the filesystem."
+    }
+    
+    Write-Output "CNAB Folder: $folder"
+
+    # Generate a Helm chart diff to the console output to see what changes will happen
+    Write-MinorStep "Printing Helm diff..."
+    Invoke-WithEcho helm diff upgrade --debug $app $folder/$app/ `
+      -f $valuesFilePath `
+      --namespace $app `
+      --install `
+      --normalize-manifests
+       
+    # Invoke Helm upgrade
+    Write-MinorStep "Running Helm upgrade..."
+    Invoke-WithEcho helm upgrade --debug $app $folder/$app/ `
+      -f $valuesFilePath `
+      --namespace $app `
+      --install `
+      --create-namespace `
+      --wait `
+      --timeout 5m `
+      @forceUpgrade
+
+    Write-Output "$($PSStyle.Foreground.BrightGreen)Installation complete!$($PSStyle.Reset)"
+  }
+
+  "post-install" { Write-Output "Post-install action" }
+  "uninstall" { Write-Output "Uninstall action" }
+  "status" { Write-Output "Status action" }
+  "human-intervention" {
+    Write-Output "Human intervention action. Exiting with 5 as a test"
+    exit 5
+  }
+  default { Write-Output "No action for $action" }
+}
+
+Write-Output "Action $action complete"
